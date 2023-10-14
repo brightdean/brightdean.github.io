4679ce16c3f8cbcb12dc985230ec84071321ee6d
Konstantinosvr <it21712@hua.gr>
2023-10-14 21:39:57 +0300

    added out-of-time and success handling

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import './Timer.css';
 
 const Timer = ({setStatus, pairs}) => {
-  const [seconds, setSeconds] = useState(2);
+  const [seconds, setSeconds] = useState(pairs * 3);
 
   useEffect(() => {
     const timer = setInterval(() => {
